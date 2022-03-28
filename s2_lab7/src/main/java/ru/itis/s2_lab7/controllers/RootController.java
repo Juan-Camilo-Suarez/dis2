feafc261800873cdfe9dd64864f92111ec86ce24
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-03-28 20:07:17 +0300

    intentos

--- s2_lab7/src/main/java/ru/itis/s2_lab7/controllers/RootController.java
+++ s2_lab7/src/main/java/ru/itis/s2_lab7/controllers/RootController.java
@@ -5,17 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.itis.s2_lab7.repositories.UserRepository;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 
 @Controller
 public class RootController {
 
-    @Autowired
-    private UserRepository repository;
 
     @GetMapping("/")
     public String getIndex(Model model) {
+        EntityManagerFactory enf = Persistence.createEntityManagerFactory("lab7");
+        enf.close();
         model.addAttribute("user", "user");
         return "index";
     }
